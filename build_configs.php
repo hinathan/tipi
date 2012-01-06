cff410d0268fbc9a14bebb4f5d6433265f5f4b21
Nathan Schmidt <nathan@Purr.local>
2012-01-06 12:20:47 -0800

    alternate port for non-public routes

--- build_configs.php
+++ build_configs.php
@@ -3,6 +3,8 @@
 
 define('kWebRootRoot','/Users/nathan/github/');
 define('kOutputRoot',kWebRootRoot . 'tipi/public');
+define('kBasePort',80);
+define('kAlternatePort',80);
 if(!is_dir(kOutputRoot)) {
 	mkdir(kOutputRoot);
 }
@@ -21,7 +23,7 @@ function buildConfigs() {
 	$path = kWebRootRoot . '*';
 
 	foreach(glob($path) as $entity) {
-
+		$port = kBasePort;
 		if(preg_match('/^\./',basename($entity))) {
 			continue;
 		}
@@ -30,21 +32,25 @@ function buildConfigs() {
 		}
 		$webroot = $entity . '/public';
 		if(!is_dir($webroot)) {
-			print "Skipping: No public path in " . basename($entity) . "\n";
+			$port = kAlternatePort;
+			print "No public path in " . basename($entity) . "\n";
 			if(is_dir($entity . '/.git')) {
 				print "\t (BUT .git DOES EXIST)\n";
+				print "\t  ... so 'hiding' at port $hostname:$port\n";
+				$webroot = $entity;
+			} else {
+				continue;
 			}
-			continue;
 		}
 		$hostname = preg_replace('/[^a-z\d]/','-',strtolower(basename($entity)));
-		$hosts_template .= createHost($hostname,$webroot);
+		$hosts_template .= createHost($hostname,$webroot,$port);
 	}
 
 	$outhostfile = kOutputRoot . "/hosts";
 	file_put_contents($outhostfile,$hosts_template);
 }
 
-function createHost($hostname, $webroot) {
+function createHost($hostname, $webroot, $port) {
 	$localhostname = $hostname . '.local';
 
 	$hosts_entry = "\n127.0.0.1\t$localhostname\n::1\t\t$localhostname\n";
@@ -52,7 +58,7 @@ function createHost($hostname, $webroot) {
 	$webrootescaped = escapeshellarg($webroot);
 
 	$httpd_template = <<<EOT
-<VirtualHost *:80>
+<VirtualHost *:$port>
 	ServerName $localhostname
 	ServerAlias *.$localhostname
 	DocumentRoot $webrootescaped
@@ -60,7 +66,24 @@ function createHost($hostname, $webroot) {
 		AllowOverride All
 		Allow from all
 		Options -MultiViews
+
+		<FilesMatch ".(ico|epub)$">
+			ExpiresActive On
+			ExpiresDefault "access plus 10 years"
+		</FilesMatch>
 	</Directory>
+	<LocationMatch "^/assets/.*$">
+		# Some browsers still send conditional-GET requests if there's a
+		# Last-Modified header or an ETag header even if they haven't
+		# reached the expiry date sent in the Expires header.
+		Header unset Last-Modified
+		Header unset ETag
+		FileETag None
+		# RFC says only cache for 1 year
+		ExpiresActive On
+		ExpiresDefault "access plus 1 year"
+	</LocationMatch>
+	
 </VirtualHost>
 
 EOT;
